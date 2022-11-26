904575c1d7f9edeb58aba7a3dc1aa7e912913bba
tobiashiro <t.felczynski@gmail.com>
2022-11-26 10:32:12 +0100

    lab3 makefile

---
+++ lab3/source/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import re
+import os
+import time
+from datetime import datetime, timedelta
+from urllib.parse import urlparse, parse_qs
+
+
+#print('source code for "http.server":', http.server.__file__)
+
+class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        path = urlparse(self.path)
+        query_params = parse_qs(path.query) 
+        
+        if path.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()            
+            if query_params.get('str', None) == ['stringas']:
+                string_to_operate = query_params.get('str', None)[0]
+                self.wfile.write(str.encode(string_to_operate))
+
+        
+        else:
+            super().do_GET()
+    
+# --- main ---
+
+PORT = 4080
+
+print(f'Starting: http://localhost:{PORT}')
+
+tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server.serve_forever()
